4791150f560274053c27885c1cbe6c3933c259fe
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-15 11:28:18 +0200

    Fix cors error

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-// const express = require('express');
 import express from "express";
+import cors from "cors";
+
 const app = express();
 const PORT = 3001;
 
+console.log("Starting app...");
+
+app.use(
+	cors({
+		origin: "*",
+	})
+);
+
 app.get("/api", (req, res) => {
-  res.json({ message: "My backend..." });
+	res.json({ message: "My backend..." });
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+	console.log(`Backend running on http://localhost:${PORT}`);
 });
